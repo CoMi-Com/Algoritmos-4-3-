7b8ecebc60442e9785ba599acd5b2d564fc293fd
CoMi-Com <braiancomini14et32@gmail.com>
2026-06-05 15:35:58 -0300

    Add files via upload

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -13,20 +13,49 @@ class consoleApp5
         int armamento = 0;
         int sector = 0;
 
-        Console.WriteLine("La guerra galáctica ha comenzado. Dos comandantes se enfrentan en un sector del espacio.\n El objetivo es eliminar a la tripulación enemiga antes de que ellos eliminen la tuya.");
+        Console.WriteLine("La guerra galáctica ha comenzado. Dos comandantes se enfrentan en un sector del espacio.\n El objetivo es eliminar a la tripulación enemiga antes de que ellos eliminen la tuya.\n\n");
         //Console.ForegroundColor = ConsoleColor.Green;
         for (int a = 0; a < 2; a++)
         {
-            Console.WriteLine($"Jugador {a+1}");
-            Console.WriteLine($"En que zona de la nave quiere distribuir tripulantes?\n Ala: [Izq] [Der]\nCápsula de: [Mando] [Motor] [Armamento]\nHabitaciones: Sector [0,1,2,3] ");
-            string zona = Console.ReadLine();
-            Console.WriteLine("Cuantos quiere distribuir?");
+            Console.WriteLine($"Jugador {a+1}\n");
+            Console.WriteLine($"Seleccione Ala:  [Izq] [Der]\n\n");
+            string alas = Console.ReadLine();
+            Console.WriteLine("En qué zona del ala?  Cápsulas[1] o Habitaciones[2]\n\n");
+            string zonas = Console.ReadLine();
+            
             while (true)
             {
-                if(zona == "Izq" || zona == "izq")
+                if(alas == "Izq" || alas == "izq")
+                {  
+                    if(zonas == "1")
+                    {
+                        Console.WriteLine("En que cápsula?   Mando[1], Motores[2], Armamento[3]");
+                        string? cap = Console.ReadLine();
+                        if(cap == "1")
+                        {
+                            Console.WriteLine("Cuántos tripulantes ingresará?");
+                            string? trip = Console.ReadLine();
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("ERROR: Ingrese un número entero.");
+                            cap = Console.ReadLine();
+                        }
+                    }
+                    
+                }
+                else if(alas == "Der" || alas == "der")
                 {
 
+                    break;
                 }
+                else
+                {
+                    
+                }
+
+                if()
             }
         }
     }
